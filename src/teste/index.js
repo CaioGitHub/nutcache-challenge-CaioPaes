cda157736f0bcbd0e5efc788fbb315370d7a4cf2
Caio Victor <caiopvasconcelos@gmail.com>
2021-10-23 03:48:38 -0300

    update

---
+++ src/teste/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/app.css'
+
+import Button from 'react-bootstrap/Button'
+
+const second = () => {
+    return (
+        <div className="main-conteiner">
+            <header class="row">
+
+            </header>
+            <body>
+
+                <div className="container">
+                    <h2>dasdsada</h2>
+                    <form action="">
+                        <div className="btn1">
+                            <Button variant="primary">Primary</Button>{' '}
+                        </div>
+
+                        <div className="btn2">
+                            <button class="btn btn-lg btn-block btn-danger">Entrar</button>
+                        </div>
+
+                        <div className="btn3">
+                            <button class="btn btn-lg btn-block btn-danger">Entrar</button>
+                        </div>
+
+                    </form>
+
+                </div>
+            </body>
+        </div>
+    );
+}
+
+export default second;
+
